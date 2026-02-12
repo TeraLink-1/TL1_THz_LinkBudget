c376cd08e697b030417a0626d4781b4d358e9b43
rrakushkin <128992626+rrakushkin@users.noreply.github.com>
2026-02-12 13:18:33 -0500

    Uploaded Link Budget Scripts

---
+++ Link Budget Scripts/linkBudget_Simplified.m
@@ -0,0 +1,17 @@
+function p_rx_dbm = linkBudget_Simplified(p_tx_dbm, g_sat, g_dish, f_c, distance, l_abs, l_total_ptg)
+    % Input:
+    %   p_tx_dbm   [dBm]     
+    %   f_c        [Hz]
+    %   distance   [m]             
+    %   l_abs (atmospheric absorption loss)     [dB]           
+    %   Output:
+    %   p_rx_dbm   [dBm]
+  
+    
+    % Spreading loss
+    l_spr = 20*log10(distance * 1e-3) + 20*log10(f_c * 1e-9) + 92.45;
+
+
+    p_rx_dbm = p_tx_dbm + g_sat + g_dish - l_spr - l_abs-l_total_ptg;
+end
+
